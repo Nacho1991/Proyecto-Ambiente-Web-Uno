c4a91220560cf131d4abf92c5dc13ce1474b3519
Nacho1991 <ivalerio2222@hotmail.com>
2014-10-30 11:23:48 -0600

    Iniciando el primer commit del proyecto web con su respectivo famework CodeIgnaiter

---
+++ application/views/practica_message.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Practica 1 CodeIgnater</title>
+    </head>
+    <body>
+        <form>
+            <fieldset>
+                <legend>Practica</legend>
+                <label>Nombre:<input type="text" name="nombre"placeholder="Nombre"> </label>
+                <label>Apellidos: <input type="text" name="apllidos" placeholder="Apellidos"></label>
+                <input type="submit" value="Enviar" name="enviar">
+            </fieldset>
+        </form>
+    </body>
+</html>
